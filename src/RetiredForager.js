f7d47aa6f5c56d6ec87a472efd85782e07121e13
rdkerr <rogerdkerr@gmail.com>
2019-01-09 11:55:17 -0700

    First attempt at implementing

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.constructor = ForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead";
+}
+
+RetiredForagerBee.prototype.gamble = function() {
+  var treasure = 'treasure' + this.treasureChest.length;
+  this.treasureChest.push(treasure);
+}
